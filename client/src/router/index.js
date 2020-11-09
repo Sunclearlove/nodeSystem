233280e0e4ba01af0f6fa148b774458b66da0b37
Sunclearlove <19855653430@163.com>
2020-11-09 22:31:17 +0800

    form validation

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '../views/Index.vue'
+import Register from '../views/register'
+import Nofind from '../views/404'
 
 Vue.use(VueRouter)
 
@@ -13,6 +15,16 @@ const routes = [
     path: '/index',
     name: '/index',
     component: Index
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '*',
+    name: 'nofind',
+    component: Nofind
   },
 ]
 
